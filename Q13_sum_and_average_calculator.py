775ac64c23da6da34e9a53a6abf33bc919a68368
Kavya L <kavyalakshman09@gmail.com>
2026-02-22 17:35:01 +0530

    Update Q13_sum_and_average_calculator.py

--- Q13_sum_and_average_calculator.py
+++ Q13_sum_and_average_calculator.py
@@ -1,8 +1,28 @@
-n = int(input("How many numbers: "))
-total = 0
-for i in range(n):
-    num = float(input("Enter number: "))
-    total += num
-avg = total / n
-print("Sum:", total)
-print("Average:", avg)
+try:
+    count = int(input("How many numbers?"))
+
+    total = 0
+
+    for i in range(1, count + 1):
+        number = float(input(f"Enter number {i}: "))
+        
+        total += number
+
+        if i == 1:
+            maximum = number
+            minimum = number
+        else:
+            if number > maximum:
+                maximum = number
+            if number < minimum:
+                minimum = number
+
+    average = total / count
+
+    print("Sum:", total)
+    print("Average:", average)
+    print("Maximum:", maximum)
+    print("Minimum:", minimum)
+
+except:
+    print("Invalid input. Please enter valid numbers.")
