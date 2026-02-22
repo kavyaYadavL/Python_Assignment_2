5373f3e2f24b2fd0eed958c9ec5c8130066b6432
Kavya L <kavyalakshman09@gmail.com>
2026-02-22 16:36:35 +0530

    Create Qn10_number_analyzer.py

---
+++ Qn10_number_analyzer.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
+
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
