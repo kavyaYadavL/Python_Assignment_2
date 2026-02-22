8aefa10b2233684e9c65ed72a568a0bbe15ad00b
Kavya L <kavyalakshman09@gmail.com>
2026-02-22 17:22:57 +0530

    Update Q5_bill_splitter.py

--- Q5_bill_splitter.py
+++ Q5_bill_splitter.py
@@ -1,9 +1,22 @@
-bill = float(input("Total bill: "))
-people = int(input("Number of people: "))
-tax = float(input("Tax %: "))
-tip = float(input("Tip %: "))
-tax_amt = bill * tax / 100
-tip_amt = (bill + tax_amt) * tip / 100
-total = bill + tax_amt + tip_amt
-print("Total bill:", total)
-print("Per person:", total / people)
+try:
+    total_bill = float(input("Enter total bill: "))
+    people = int(input("Number of people: "))
+    tax_percent = float(input("Tax percentage: "))
+    tip_percent = float(input("Tip percentage: "))
+
+    tax_amount = (tax_percent / 100) * total_bill
+    after_tax = total_bill + tax_amount
+    tip_amount = (tip_percent / 100) * after_tax
+    final_total = after_tax + tip_amount
+    per_person = final_total / people
+
+    print("\n=== BILL BREAKDOWN ===")
+    print("Subtotal:", total_bill)
+    print("Tax:", tax_amount)
+    print("After tax:", after_tax)
+    print("Tip:", tip_amount)
+    print("Total:", final_total)
+    print("Per person:", per_person)
+
+except:
+    print("Invalid input. Please enter correct values.")
