f550b2e7d913f2c235c96571e039324c0a67f71d
Kavya L <kavyalakshman09@gmail.com>
2026-02-22 17:29:17 +0530

    Update Q12_multiplication_table_generator.py

--- Q12_multiplication_table_generator.py
+++ Q12_multiplication_table_generator.py
@@ -1,3 +1,10 @@
-num = int(input("Enter a number: "))
-for i in range(1, 11):
-    print(num * i)
+try:
+    number = int(input("Enter number: "))
+    end_range = int(input("Enter range (end): "))
+
+    print("\nMultiplication Table of", number)
+
+    for i in range(1, end_range + 1):
+        print(number, "x", i, "=", number * i)
+except:
+    print("Invalid input. Please enter valid numbers.")
